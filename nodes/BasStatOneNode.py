62a31ad536066b78603f37b82c775eff2235d1da
sjpbailey <sjpbailey1961@gmail.com>
2021-07-03 01:42:59 -0700

    Update BasStatOneNode.py

--- nodes/BasStatOneNode.py
+++ nodes/BasStatOneNode.py
@@ -144,24 +144,25 @@ class BasStatOneNode(polyinterface.Node):
             self.setDriver("GV16", 1)
             self.setDriver("GV17", 0) 
             LOGGER.info('Heat')
-        if self.modeOn == 1:
+        elif self.modeOn == 1:
             self.bc.virtualValue(5, 205, 0)
             self.bc.virtualValue(6, 206, 0)
             self.setDriver("GV16", 0)
             self.setDriver("GV17", 0) 
             LOGGER.info('Off')
-        if self.modeOn == 2:
+        elif self.modeOn == 2:
             self.bc.virtualValue(5, 205, 0)
             self.bc.virtualValue(6, 206, 1)
             self.setDriver("GV16", 0)
             self.setDriver("GV17", 1)
             LOGGER.info('Cool')
-        if self.modeOn == 3:
-            self.bc.virtualValue(5, 205, 1)
-            self.bc.virtualValue(6, 206, 1)
-            self.setDriver("GV16", 1)
-            self.setDriver("GV17", 1) 
-            LOGGER.info('Auto')
+        else:
+            if self.modeOn == 3:
+                self.bc.virtualValue(5, 205, 1)
+                self.bc.virtualValue(6, 206, 1)
+                self.setDriver("GV16", 1)
+                self.setDriver("GV17", 1) 
+                LOGGER.info('Auto')
         return    
     
     # Heating Setpoint
