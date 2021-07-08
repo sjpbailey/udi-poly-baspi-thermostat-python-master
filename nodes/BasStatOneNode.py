b4bcacbd82f0d28dc3d79cd3f873f0ea3fd38207
sjpbailey <sjpbailey1961@gmail.com>
2021-07-07 22:52:52 -0700

    Update BasStatOneNode.py

--- nodes/BasStatOneNode.py
+++ nodes/BasStatOneNode.py
@@ -248,7 +248,7 @@ class BasStatOneNode(polyinterface.Node):
         {'driver': 'GV16', 'value': 1, 'uom': 25}, # Virtual Value VT-5 Heat Enable
         {'driver': 'GV17', 'value': 1, 'uom': 25}, # Virtual Value VT-6 Cool Enable
         {'driver': 'GV18', 'value': 0, 'uom': 25}, # Virtual Value VT-7 Aux Enable
-        #{'driver': 'CLISMD', 'value': 'self.setsch', 'uom': 25}, # For Schedual OVRD
+        {'driver': 'CLISMD', 'value': 'self.setsch', 'uom': 25}, # For Schedual OVRD
         #{'driver': 'GV20','value': 0, 'uom': 25}, # For Fan OVRD
         #{'driver': 'CLIFS','value': 0, 'uom': 68}, # For Aux OVRD
         #{'driver': 'CLIMD','value': "self.modeOn", 'uom': 67}, # For Mode OVRD
