cecc4ea28321de20b1a5e723d6a9fd319e1f084d
sjpbailey <sjpbailey1961@gmail.com>
2021-07-14 23:04:10 -0700

    Update BasStatOneNode.py

--- nodes/BasStatOneNode.py
+++ nodes/BasStatOneNode.py
@@ -242,7 +242,8 @@ class BasStatOneNode(polyinterface.Node):
         {'driver': 'GV11', 'value': 0, 'uom': 80}, # Aux Override BO-6
         {'driver': 'GV12', 'value': 1, 'uom': 17}, # Virtual Value VT-1 Heat SETP
         {'driver': 'GV13', 'value': 1, 'uom': 17}, # Virtual Value VT-2 Cool SETP
-        
+        {'driver': 'GV22', 'value': 1, 'uom': 17}, # Virtual Value VT-2 Cool SETP
+        {'driver': 'GV23', 'value': 1, 'uom': 17}, # Virtual Value VT-1 Heat SETP
         ##### Cycles GV's to value? So only setting the drivers above in def setVirtualDriver virtual values! #####
         #{'driver': 'GV14', 'value': 0,'uom': 25}, # Virtual Value VT-3 Schedual
         #{'driver': 'GV15', 'value': 0, 'uom': 25}, # Virtual Value VT-4 Fan Enable
