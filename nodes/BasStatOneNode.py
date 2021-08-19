63b525f462ca45f77be85bbc922c19d7ff68052c
sjpbailey <sjpbailey1961@gmail.com>
2021-08-19 13:03:55 -0700

    Update BasStatOneNode.py

--- nodes/BasStatOneNode.py
+++ nodes/BasStatOneNode.py
@@ -55,7 +55,7 @@ class BasStatOneNode(polyinterface.Node):
             LOGGER.info('BO ' + '\t' + str(self.bc.binaryOutput(i)))
         LOGGER.info('Virtual')
         for i in range(1,10):
-            for x in range(1):
+            for x in range(1,210):
                 LOGGER.info('VT '  + '\t' + str(self.bc.virtualValue(i, x)))
 
         # Set Nodes used
